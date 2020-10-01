c605de9e5b13a6d543119c80fee7822653490fbb
Vladimir Menshikov <v0v4nk95@gmail.com>
2020-10-01 03:54:05 +0300

    start

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index() => View(repository.Products);
+    }
+}
---
+++ Project/Models/StoreDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Project.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+        : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
