a98aac477ae41251b5f1469e8bd21fcd8b7d7d05
Vladimir Menshikov <v0v4nk95@gmail.com>
2020-10-08 01:59:44 +0300

    almost deploy

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Project.Models;
+using Microsoft.AspNetCore.Identity;
+
 namespace Project
 {
     public class Startup
@@ -35,14 +37,30 @@ namespace Project
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddServerSideBlazor();
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["ConnectionStrings:IdentityConnection"]));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute("catpage",
                     "{category}/Page{productPage:int}",
@@ -60,7 +78,7 @@ namespace Project
                 endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
             SeedData.EnsurePopulated(app);
-
+            IdentitySeedData.EnsurePopulated(app);
         }
     }
 }
